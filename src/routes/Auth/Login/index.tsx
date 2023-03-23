96278aea7025d2ce45eb0e0cd1261eb1f4f86106
arinugraha11 <ari.nugraha@tokopedia.com>
2023-03-23 14:37:15 +0700

    feat: implement private route

---
+++ src/routes/Auth/Login/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Username</p>
+      <input />
+      <p>Password</p>
+      <input type="password" />
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // Add your components here
+import PrivateRoutes from "../components/PrivateRoutes";
 import About from "./About";
+import Login from "./Auth/Login";
 import Home from "./Home";
 
 const MainRoutes = () => {
   return (
     <Routes>
+      <Route element={<PrivateRoutes />}>
+        {/* Add component that need authentication */}
+        <Route path="/about" element={<About />} />
+      </Route>
+
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
 
       {/* Add more routes below */}
     </Routes>
