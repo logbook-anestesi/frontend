0693169bcb171d6eff9de5591d204624c3b17c0e
arinugraha11 <ari.nugraha@tokopedia.com>
2023-07-22 17:45:40 +0700

    chore: remove log

--- src/routes/AddCases/components/FormOperation/index.tsx
+++ src/routes/AddCases/components/FormOperation/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Flex, Text, useDisclosure } from "@chakra-ui/react";
