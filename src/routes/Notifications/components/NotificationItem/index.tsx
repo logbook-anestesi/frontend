4e5ab9d1f0bc1e0f2be3919b87fa77b5e7e0a1e3
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-06 19:18:06 +0700

    feat: implement onclick notif

--- src/routes/Notifications/components/NotificationItem/index.tsx
+++ src/routes/Notifications/components/NotificationItem/index.tsx
@@ -1,13 +1,20 @@
-import { Circle, Divider, Flex, Text } from "@chakra-ui/react";
+import { Divider, Flex, Text } from "@chakra-ui/react";
 import { Notification } from "../../hooks/useGetAllNotifications/types";
 import { convertDateForNotification } from "../../../../helpers";
 import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   notification: Notification;
 }
 
 const NotificationItem = ({ notification }: Props) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(notification?.redirectUrl || "/");
+  };
+
   const parsedDocument = useMemo(() => {
     const parser = new DOMParser();
 
@@ -16,12 +23,13 @@ const NotificationItem = ({ notification }: Props) => {
   }, [notification?.description]);
 
   return (
-    <Flex direction="column" gap="4px">
-      <Text fontSize="xs" align="end">
+    <Flex direction="column" gap="4px" onClick={handleNavigate}>
+      <Text fontSize="xs" align="end" mt={2}>
         {convertDateForNotification(notification?.created)}
       </Text>
 
-      {!notification?.isRead && <Circle size="10px" bg="tomato" />}
+      {/* Temporary disable, waiting for BE API */}
+      {/* {!notification?.isRead && <Circle size="10px" bg="tomato" />} */}
 
       <Text as="b" fontSize="sm">
         {notification?.title}
