2884be8e5c884a78ee4397d1358702af7720c035
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-06 12:37:28 +0700

    feat: change icon profile in home

--- src/routes/Home/components/HeaderHome/index.tsx
+++ src/routes/Home/components/HeaderHome/index.tsx
@@ -1,7 +1,8 @@
 import { Flex, Image } from "@chakra-ui/react";
-import { AtSignIcon, BellIcon } from "@chakra-ui/icons";
+import { BellIcon } from "@chakra-ui/icons";
 import { colors } from "../../../../constants/colors";
 import logbookIcon from "../../../../assets/logbook-icon.svg";
+import profileIcon from "../../assets/profile.png";
 import { useNavigate } from "react-router-dom";
 
 const HeaderHome = () => {
@@ -13,13 +14,14 @@ const HeaderHome = () => {
 
   return (
     <Flex justify="space-between" width="100%" marginBottom="32px">
-      <AtSignIcon
+      <Image
         width={33}
         height={33}
         backgroundColor={colors.lightGrey}
         borderRadius="10px"
         padding="8px"
         color={colors.primaryPurple}
+        src={profileIcon}
       />
 
       <Image src={logbookIcon} maxWidth="109px" />
