3b389a31497cd06fe7e1f0d2f40c3d4137b7205a
Nur Fauziah Hasanah <nur.hasanah@traveloka.com>
2023-08-06 10:21:02 +0700

    Implement competence feature and enhance homepage (#12)
    * Implement competence feature and enhance homepage
    
    * Wire competence page
    
    * Fix failing build

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -4,11 +4,12 @@ import Profile from "./components/Profile";
 import doctorIcon from "../../assets/doctor.png";
 import InfoBox from "../../components/InfoBox";
 import LevelCard from "./components/LevelCard";
+import LevelCardWithLogo from "./components/LevelCardWithLogo";
 import ReportCard from "./components/ReportCard";
 import cases from "./assets/cases.png";
 import exam from "./assets/exam.png";
 import ilmiah from "./assets/ilmiah.png";
-import ButtonTambah from "./components/ButtonTambah";
+import ButtonTambah from "../../components/ButtonTambah";
 import useGetProfile from "../../hooks/useGetProfile";
 
 const Home = () => {
@@ -33,11 +34,14 @@ const Home = () => {
 
       <Flex direction="column" gap="10px">
         <LevelCard
-          title="Residen Pembekalan"
+          title="Pembekalan"
           type="Level Kompetensi"
-          path="/"
+          path="/competence"
         />
         <LevelCard title="Obgyn" type="Stase" path="/stase" />
+        <LevelCardWithLogo title="Cases" type="Pending Review" path="/review/cases" icon={cases} cardNumber={1}/>
+        <LevelCardWithLogo title="Ilmiah & Exam" type="Pending Review" path="/stase" icon={cases} cardNumber={1}/>
+        <LevelCardWithLogo title="Uro-Gimul" type="Dashboard Stase" path="/dashboard/station" icon={cases} cardNumber={1}/>
       </Flex>
 
       <Flex justify="space-between" gap="15px">
@@ -46,7 +50,7 @@ const Home = () => {
         <ReportCard icon={exam} title="Exam" path="/" />
       </Flex>
 
-      <ButtonTambah />
+      <ButtonTambah buttonTitle="Tambah Cases" />
     </Flex>
   );
 };
