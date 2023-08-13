9cd500221aa0ade530219860a37914fec41a8d3a
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-13 15:07:38 +0700

    feat: add modal confirm approve

--- src/routes/ApprovingProcess/index.tsx
+++ src/routes/ApprovingProcess/index.tsx
@@ -1,10 +1,12 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, useDisclosure } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import RadioPenilaian from "./components/RadioPenilaian";
 import TextPenilaian from "./components/TextPenilaian";
 import { colors } from "../../constants/colors";
+import ModalApprove from "./components/ModalApprove";
 
 const ApprovingProcess = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <Flex flexDirection="column">
       <Header pathBack="/" title="Approve OK-014-01" />
@@ -18,10 +20,14 @@ const ApprovingProcess = () => {
           backgroundColor={colors.primaryPurple}
           color={colors.white}
           mt={10}
+          onClick={onOpen}
         >
           Approve
         </Button>
       </Flex>
+
+      {/* Modal Section */}
+      <ModalApprove closeModal={onClose} isOpen={isOpen} />
     </Flex>
   );
 };
