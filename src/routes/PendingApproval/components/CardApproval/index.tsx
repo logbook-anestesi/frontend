a5319678208c56cac8c35047b5ea27194198afb6
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-13 14:29:40 +0700

    feat: create ui for approval konsulen

--- src/routes/PendingApproval/components/CardApproval/index.tsx
+++ src/routes/PendingApproval/components/CardApproval/index.tsx
@@ -1,23 +1,33 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Divider, Flex, Image, Text } from "@chakra-ui/react";
 import profileIcon from "../../assets/profileIcon.png";
 import { colors } from "../../../../constants/colors";
+import ButtonFile from "../ButtonFile";
+import CardApprovalButton from "../CardApprovalButton";
 
 const CardApproval = () => {
   return (
-    <Flex direction="column">
+    <Flex direction="column" mb={3}>
       <Text align="right" color={colors.darkGrey} fontSize="sm">
         30/03 17.00
       </Text>
 
-      <Flex direction="column" gap={1}>
-        <Text as="b">Cases: OK/Surgery</Text>
-        <Flex align="center" gap={1}>
-          <Image src={profileIcon} alt="" width={3} height={4} />
-          <Text color={colors.darkGrey} fontSize="sm">
-            dr. Dian Sastrowardoyo
-          </Text>
+      <Flex direction="column" gap={3} mb={1}>
+        <Flex direction="column">
+          <Text as="b">Cases: OK/Surgery</Text>
+          <Flex align="center" gap={1}>
+            <Image src={profileIcon} alt="" width={3} height={4} />
+            <Text color={colors.darkGrey} fontSize="sm">
+              dr. Dian Sastrowardoyo
+            </Text>
+          </Flex>
         </Flex>
+
+        <ButtonFile title="C-OK24-L" />
       </Flex>
+
+      <CardApprovalButton />
+
+      <Divider mt={5} />
     </Flex>
   );
 };
