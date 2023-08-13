365f33e9ba8f9f5f0c0f163555a831ea01f3bff9
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-13 23:01:37 +0700

    feat: remove double routes and integrate cases review

---
+++ src/routes/CasesReviewDashboard/hooks/useGetPendingReview/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../../../../networks/apiClient";
+import { ReviewItem } from "./types";
+
+const useGetPendingReview = () => {
+  const [loading, setLoading] = useState(false);
+  const [reviewData, setReviewData] = useState<ReviewItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const response = await axiosClient.get("/cases/approval/");
+      const data = await response.data.data;
+
+      setLoading(false);
+      setReviewData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return {
+    reviewData,
+    loading,
+  };
+};
+
+export default useGetPendingReview;
--- src/routes/CasesReviewDashboard/index.tsx
+++ src/routes/CasesReviewDashboard/index.tsx
@@ -1,12 +1,33 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import Header from "../../components/Header";
+import useGetPendingReview from "./hooks/useGetPendingReview";
+import { colors } from "../../constants/colors";
+import CardApproval from "./components/CardApproval";
 
 const CasesReviewDashboardPage = () => {
-    return (
-        <Flex flexDirection="column">
-        <Header pathBack="/" title="Pending Cases Review" />
+  const { reviewData } = useGetPendingReview();
+
+  console.log("999 REVIEW DATA", reviewData);
+
+  return (
+    <Flex flexDirection="column">
+      <Header pathBack="/" title="Pending Cases Review" />
+
+      <Flex padding="30px" direction="column" gap="16px">
+        <Button
+          colorScheme="teal"
+          backgroundColor={colors.primaryPurple}
+          color={colors.white}
+        >
+          Approve All
+        </Button>
+
+        <CardApproval />
+        <CardApproval />
+        <CardApproval />
       </Flex>
-    );
+    </Flex>
+  );
 };
 
-export default CasesReviewDashboardPage
+export default CasesReviewDashboardPage;
