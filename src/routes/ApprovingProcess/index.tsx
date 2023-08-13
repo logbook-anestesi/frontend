92443ebc7267285c42e83953fe350e98ceb103e6
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-13 14:58:09 +0700

    feat: add button approve

--- src/routes/ApprovingProcess/index.tsx
+++ src/routes/ApprovingProcess/index.tsx
@@ -1,7 +1,8 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import RadioPenilaian from "./components/RadioPenilaian";
 import TextPenilaian from "./components/TextPenilaian";
+import { colors } from "../../constants/colors";
 
 const ApprovingProcess = () => {
   return (
@@ -11,6 +12,15 @@ const ApprovingProcess = () => {
       <Flex padding="30px" direction="column" gap="16px">
         <RadioPenilaian />
         <TextPenilaian />
+
+        <Button
+          colorScheme="teal"
+          backgroundColor={colors.primaryPurple}
+          color={colors.white}
+          mt={10}
+        >
+          Approve
+        </Button>
       </Flex>
     </Flex>
   );
