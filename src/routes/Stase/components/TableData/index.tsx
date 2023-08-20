449cbef34d1a9825a4f393f24b80736a2251fc6a
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-20 16:34:17 +0700

    feat: implement data table

---
+++ src/routes/Stase/components/TableData/index.tsx
@@ -0,0 +1,57 @@
+import DataTable, { TableColumn } from "react-data-table-component";
+import { useMemo } from "react";
+import { formatMonthYear } from "../../../../helpers";
+import { StaseUser } from "../../hooks/useGetStaseUser/types";
+
+interface DataRow {
+  index: number;
+  stationName: string;
+  date: string;
+  status: string;
+}
+
+interface Props {
+  stationList: StaseUser[];
+  loading: boolean;
+}
+
+const TableData = ({ stationList }: Props) => {
+  const columns: TableColumn<DataRow>[] = [
+    {
+      name: "No",
+      selector: (row) => row.index,
+      sortable: true,
+      maxWidth: "2px",
+    },
+    {
+      name: "Nama",
+      selector: (row) => row.stationName,
+      sortable: true,
+    },
+    {
+      name: "Tanggal",
+      selector: (row) => row.date,
+      sortable: true,
+    },
+    {
+      name: "Status",
+      selector: (row) => row.status,
+      sortable: true,
+    },
+  ];
+
+  const data = useMemo(() => {
+    return stationList.map((singleStation, idx) => {
+      return {
+        index: idx + 1,
+        stationName: singleStation.stationName,
+        date: formatMonthYear(singleStation.periodMmYyyy),
+        status: "Lulus",
+      };
+    });
+  }, [stationList]);
+
+  return <DataTable columns={columns} data={data} pagination />;
+};
+
+export default TableData;
---
+++ src/routes/StaseAllList/components/TableData/index.tsx
@@ -0,0 +1,49 @@
+import DataTable, { TableColumn } from "react-data-table-component";
+import { useMemo } from "react";
+import { Stase } from "../../hooks/useGetAllStase/types";
+
+interface DataRow {
+  index: number;
+  stationName: string;
+  leaderName: string;
+}
+
+interface Props {
+  stationList: Stase[];
+  loading: boolean;
+}
+
+const TableData = ({ stationList, loading }: Props) => {
+  const columns: TableColumn<DataRow>[] = [
+    {
+      name: "Nomor",
+      selector: (row) => row.index,
+      sortable: true,
+      maxWidth: "5px",
+    },
+    {
+      name: "Nama Stase",
+      selector: (row) => row.stationName,
+      sortable: true,
+    },
+    {
+      name: "Ketua Modul",
+      selector: (row) => row.leaderName,
+      sortable: true,
+    },
+  ];
+
+  const data = useMemo(() => {
+    return stationList.map((singleStation, idx) => {
+      return {
+        index: idx + 1,
+        stationName: singleStation.stationName,
+        leaderName: singleStation.leaderName,
+      };
+    });
+  }, [stationList]);
+
+  return <DataTable columns={columns} data={data} pagination />;
+};
+
+export default TableData;
