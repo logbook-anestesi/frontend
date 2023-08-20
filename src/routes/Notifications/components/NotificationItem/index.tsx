053f666bfc066dff483ac1ffcc7dc97e92757a3e
arinugraha11 <ari.nugraha@tokopedia.com>
2023-08-20 10:27:03 +0700

    feat: read notifications

--- src/routes/Notifications/components/NotificationItem/index.tsx
+++ src/routes/Notifications/components/NotificationItem/index.tsx
@@ -1,18 +1,40 @@
-import { Divider, Flex, Text } from "@chakra-ui/react";
+import { Divider, Flex, Text, useToast } from "@chakra-ui/react";
 import { Notification } from "../../hooks/useGetAllNotifications/types";
 import { convertDateForNotification } from "../../../../helpers";
 import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+import useReadNotifications from "../../hooks/useReadNotifications";
+import LoaderCircle from "../../../../components/LoaderCircle";
 
 interface Props {
   notification: Notification;
 }
 
 const NotificationItem = ({ notification }: Props) => {
+  const toast = useToast();
   const navigate = useNavigate();
+  const { loading, readNotification } = useReadNotifications();
 
-  const handleNavigate = () => {
-    navigate(notification?.redirectUrl || "/");
+  const handleClick = async () => {
+    const response = await readNotification(notification?.id);
+
+    if (response?.success) {
+      navigate(notification?.redirectUrl || "/");
+      return;
+    }
+
+    if (!response?.success) {
+      toast({
+        title: "Failed Read Notification",
+        description: response?.message,
+        status: "error",
+        position: "top",
+        duration: 5000,
+        isClosable: true,
+      });
+
+      return;
+    }
   };
 
   const parsedDocument = useMemo(() => {
@@ -22,8 +44,12 @@ const NotificationItem = ({ notification }: Props) => {
       .innerHTML;
   }, [notification?.description]);
 
+  if (loading) {
+    return <LoaderCircle />;
+  }
+
   return (
-    <Flex direction="column" gap="4px" onClick={handleNavigate}>
+    <Flex direction="column" gap="4px" onClick={handleClick}>
       <Text fontSize="xs" align="end" mt={2}>
         {convertDateForNotification(notification?.created)}
       </Text>
---
+++ src/routes/Notifications/hooks/useReadNotifications/index.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react";
+import axiosClient from "../../../../networks/apiClient";
+
+const useReadNotifications = () => {
+  const [loading, setLoading] = useState(false);
+
+  const readNotification = useCallback(async (notificationId: string) => {
+    setLoading(true);
+
+    try {
+      const response = await axiosClient.post(
+        `/notification/read/${notificationId}`
+      );
+      const data = response.data;
+
+      setLoading(false);
+
+      if (data?.error) {
+        return { success: false, message: data?.message[0] };
+      }
+
+      if (!data.error) {
+        return {
+          success: true,
+          message: "Success Read Notification",
+        };
+      }
+    } catch (e: any) {
+      setLoading(false);
+      console.log("[Error Approve Case]", e);
+      return { success: false, message: e?.response?.data?.message };
+    }
+  }, []);
+
+  return {
+    loading,
+    readNotification,
+  };
+};
+
+export default useReadNotifications;
