e6bb35f8ebad6ea50315490cf9e5b2868f3e9846
arinugraha11 <ari.nugraha@tokopedia.com>
2023-09-10 17:40:15 +0700

    feat: add button ganti foto

---
+++ src/routes/Profile/hooks/useUploadProfilePhoto/index.tsx
@@ -0,0 +1,43 @@
+import { useCallback, useState } from "react";
+import { PayloadUploadPhoto } from "./types";
+import axiosClient from "../../../../networks/apiClient";
+
+const useUploadProfilePhoto = () => {
+  const [loading, setLoading] = useState(false);
+
+  const uploadPhoto = useCallback(async (payload: PayloadUploadPhoto) => {
+    setLoading(true);
+
+    try {
+      const response = await axiosClient.post(
+        "/auth/update-profile-photo",
+        payload
+      );
+      const data = response.data;
+
+      setLoading(false);
+
+      if (data?.error) {
+        return { success: false, message: data?.message[0] };
+      }
+
+      if (!data.error) {
+        return {
+          success: true,
+          message: "Succes Upload Photo",
+        };
+      }
+    } catch (e: any) {
+      setLoading(false);
+      console.log("[Error Upload Photo]", e);
+      return { success: false, message: e?.response?.data?.message };
+    }
+  }, []);
+
+  return {
+    loading,
+    uploadPhoto,
+  };
+};
+
+export default useUploadProfilePhoto;
--- src/routes/Profile/index.tsx
+++ src/routes/Profile/index.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 
 import doctorIcon from "../../assets/doctor.png";
 import useGetProfile from "../../hooks/useGetProfile";
@@ -6,11 +14,15 @@ import Header from "../../components/Header";
 import { colors } from "../../constants/colors";
 import LoaderCircle from "../../components/LoaderCircle";
 import useAuth from "../../hooks/useAuth";
-import { useMemo } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
+import useUploadProfilePhoto from "./hooks/useUploadProfilePhoto";
 
 const ProfilePage = () => {
+  const toast = useToast();
   const { loading, profile } = useGetProfile();
   const { logoutAccount } = useAuth();
+  const [selectedPhoto, setSelectedPhoto] = useState<string | null>(null);
+  const { uploadPhoto } = useUploadProfilePhoto();
 
   const selectedProfileData = [
     {
@@ -70,6 +82,63 @@ const ProfilePage = () => {
     return profile?.role === "KONSULEN";
   }, [profile?.role]);
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setSelectedPhoto(event.target?.result as string);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setSelectedPhoto(null);
+    }
+  };
+
+  const handleSubmitPhotoProfile = async () => {
+    const response = await uploadPhoto({
+      imageUrl: selectedPhoto || "",
+    });
+
+    if (response?.success) {
+      toast({
+        title: "Success",
+        description: "Success Upload Photo",
+        status: "success",
+        position: "top",
+        duration: 5000,
+        isClosable: true,
+      });
+
+      setSelectedPhoto(null);
+      return;
+    }
+
+    if (!response?.success) {
+      toast({
+        title: "Failed Upload Photo",
+        description: response?.message,
+        status: "error",
+        position: "top",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const profileImage = () => {
+    if (selectedPhoto === null) {
+      return profile?.imageUrl || doctorIcon;
+    }
+
+    return selectedPhoto;
+  };
+
+  useEffect(() => {
+    console.log("999 selected photo", selectedPhoto);
+  }, [selectedPhoto]);
+
   if (loading) {
     return (
       <Flex direction="column">
@@ -85,15 +154,32 @@ const ProfilePage = () => {
       <Header title="Profile" />
       <Flex direction="column" gap="38px" padding="30px">
         <Flex direction="column" align="center" gap="8px">
-          <Image
-            src={profile?.imageUrl || doctorIcon}
-            width="80px"
-            height="80px"
-            mb={3}
+          <Image src={profileImage()} width="80px" height="80px" mb={3} />
+
+          <label htmlFor="file-input">
+            <Text as="b" fontSize="xs" color={colors.primaryPurple}>
+              Ganti Foto
+            </Text>
+          </label>
+
+          {selectedPhoto !== null && (
+            <Button
+              color={colors.white}
+              fontSize="xs"
+              py={0}
+              bgColor={colors.primaryPurple}
+              onClick={handleSubmitPhotoProfile}
+            >
+              Ganti
+            </Button>
+          )}
+
+          <Input
+            type="file"
+            display="none"
+            id="file-input"
+            onChange={handleFileChange}
           />
-          <Text as="b" fontSize="xs" color={colors.primaryPurple}>
-            Ganti Foto
-          </Text>
         </Flex>
 
         {!isKonsulen && (
