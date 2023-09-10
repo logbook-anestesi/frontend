e2c47bb5720661b0655ccb5ce66d9ca13474bba5
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-09-10 21:04:46 +0700

    add get all case

--- src/routes/Cases/hooks/useGetCases/index.tsx
+++ src/routes/Cases/hooks/useGetCases/index.tsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from "react";
 import axiosClient from "../../../../networks/apiClient";
 import { Case } from "./types";
+import useSWR from "swr";
 
+// TODO: mutate mechanism
 const useGetCases = () => {
-  const [loading, setLoading] = useState(false);
-  const [caseList, setCaseList] = useState<Case[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-
-      const response = await axiosClient.get("/cases");
-      const data = await response.data.data;
-
-      setLoading(false);
-      setCaseList(data);
-    };
-
-    fetchData();
-  }, []);
+  const { data: caseList, isLoading: loading } = useSWR(`/cases`, async (): Promise<Case[]> => {
+    const response = await axiosClient.get("/cases");
+    return response.data.data;
+  })
 
   return {
     caseList,
