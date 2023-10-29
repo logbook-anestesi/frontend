8710e64a916a06425b98b720a19580f8e8f32f5a
arinugraha11 <ari.nugraha@tokopedia.com>
2023-10-29 09:54:56 +0700

    feat: implement unread notification

--- src/routes/Home/components/HeaderHome/index.tsx
+++ src/routes/Home/components/HeaderHome/index.tsx
@@ -3,10 +3,20 @@ import { BellIcon } from '@chakra-ui/icons';
 import { colors } from '../../../../constants/colors';
 import logbookIcon from '../../../../assets/logbook-icon.svg';
 import profileIcon from '../../assets/profile.png';
+import unreadBell from '../../assets/notif-unread.png';
 import { useNavigate } from 'react-router-dom';
+import useGetAllNotifications from '../../../Notifications/hooks/useGetAllNotifications';
+import { useMemo } from 'react';
 
 const HeaderHome = () => {
+  const { notifications } = useGetAllNotifications();
   const navigate = useNavigate();
+
+  const isHaveUnreadNotification = useMemo(() => {
+    const unreadNotif = notifications?.find((notif) => notif.isRead === false);
+
+    return unreadNotif ? true : false;
+  }, [notifications]);
 
   const handleNotification = () => {
     navigate('/notifications');
@@ -40,15 +50,28 @@ const HeaderHome = () => {
 
       <Image src={logbookIcon} maxWidth="109px" />
 
-      <BellIcon
-        width={33}
-        height={33}
-        backgroundColor={colors.lightGrey}
-        borderRadius="10px"
-        padding="8px"
-        color={colors.primaryPurple}
-        onClick={handleNotification}
-      />
+      {isHaveUnreadNotification ? (
+        <Image
+          width={33}
+          height={33}
+          backgroundColor={colors.lightGrey}
+          borderRadius="10px"
+          padding="3px"
+          color={colors.primaryPurple}
+          src={unreadBell}
+          onClick={handleNotification}
+        />
+      ) : (
+        <BellIcon
+          width={33}
+          height={33}
+          backgroundColor={colors.lightGrey}
+          borderRadius="10px"
+          padding="8px"
+          color={colors.primaryPurple}
+          onClick={handleNotification}
+        />
+      )}
     </Flex>
   );
 };
