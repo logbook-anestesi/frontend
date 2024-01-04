e9f8e84b1589fa89d2c5952d95477b6daa85c615
Nur Fauziah Hasanah <nur.hasanah@traveloka.com>
2024-01-04 21:47:35 +0700

    improvement: add pathvariable userId in get all pelanggaran

--- src/routes/Pelanggaran/components/TableListPelanggaran/index.tsx
+++ src/routes/Pelanggaran/components/TableListPelanggaran/index.tsx
@@ -2,9 +2,12 @@ import { Flex, Text } from '@chakra-ui/react';
 import useGetAllPelanggaran from '../../hooks/useGetAllPelanggaran';
 import LoaderCircle from '../../../../components/LoaderCircle';
 import TableData from '../TableData';
+import useGetProfile from '../../../../hooks/useGetProfile';
 
 const TableListPelanggaran = () => {
-  const { loading, pelanggaranList } = useGetAllPelanggaran();
+  const { profile } = useGetProfile();
+  const userId = profile ? profile.id : '';
+  const { loading, pelanggaranList } = useGetAllPelanggaran(userId);
 
   return (
     <Flex direction="column">
