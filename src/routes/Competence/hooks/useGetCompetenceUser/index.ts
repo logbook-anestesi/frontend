ae072a7212798ae5f93ab52ab1cf39baa2d82523
Nur Fauziah Hasanah <nur.hasanah@traveloka.com>
2024-03-12 07:19:51 +0700

    feat: disable to post new request if pending competencerequest exist

--- src/routes/Competence/hooks/useGetCompetenceUser/index.ts
+++ src/routes/Competence/hooks/useGetCompetenceUser/index.ts
@@ -5,16 +5,20 @@ import { CompetenceUser } from './types';
 const useGetCompetenceUser = () => {
   const [loading, setLoading] = useState(false);
   const [rawCompetenceData, setCompetenceData] = useState<CompetenceUser[]>([]);
+  const [isAllowedToMakeNewRequest, setIsAllowedToMakeNewRequest] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
 
       const response = await axiosClient.get('/competence/all');
-      const data = await response.data.data;
+      const responseData = await response.data.data;
+      const data = responseData.competences;
+      const allowedToMakeNewRequest = responseData.isAllowedToMakeNewRequest
 
       setCompetenceData(data);
       setLoading(false);
+      setIsAllowedToMakeNewRequest(allowedToMakeNewRequest)
     };
 
     fetchData();
@@ -31,6 +35,7 @@ const useGetCompetenceUser = () => {
   return {
     competenceData,
     loading,
+    isAllowedToMakeNewRequest
   };
 };
 
