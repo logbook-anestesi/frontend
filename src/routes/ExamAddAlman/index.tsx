705d57639e0b7f174328e216ce13725e0c4fdc20
arinugrahaa <arinugraha@tokopedia.com>
2024-04-04 10:14:47 +0700

    feat: add ui alman date

--- src/routes/ExamAddAlman/index.tsx
+++ src/routes/ExamAddAlman/index.tsx
@@ -1,12 +1,18 @@
 import { Flex } from '@chakra-ui/react';
 import Header from '../../components/Header';
+import { useState } from 'react';
+import FormDate from '../ExamAdd/components/FormDate';
 
 const ExamAddAlman = () => {
+  const [date, setDate] = useState('');
+
   return (
     <Flex direction="column">
       <Header title="Tambah ALMAN" />
 
-      <Flex padding="10px 30px" direction="column" gap="16px"></Flex>
+      <Flex padding="10px 30px" direction="column" gap="16px">
+        <FormDate setDate={setDate} />
+      </Flex>
     </Flex>
   );
 };
