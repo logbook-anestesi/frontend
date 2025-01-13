137618f49d47bf220dbae6b705440319d9f5fe07
Nur Fauziah Hasanah <nur.fauziah81@ui.ac.id>
2025-01-13 07:57:14 +0700

    Update index.tsx

--- src/routes/Exam/components/TableDataPreparation/index.tsx
+++ src/routes/Exam/components/TableDataPreparation/index.tsx
@@ -8,7 +8,20 @@ import { ExamPreparation } from '../../hooks/useGetAllExamPreparation/types';
 interface DataRow {
   idx: number;
   type: string;
+  procedure: string | '';
+  reason: string | '';
+  approvalLocation: string | '';
+  approvalDifficulty: string | '';
   approvalDate: string;
+  approvalSupervision: string | '';
+  preparationDate: string | '';
+  selfEvaluation: string | '';
+  isGoingWell: string | '';
+  selfReflection: string | '';
+  approvalGlobalRating: string | '';
+  approvalFeedbackGiven: string | '';
+  operationTypes: any[];
+  additionalTags: any[];
   accessor: string;
   status: string;
   createdDate: string;
@@ -32,31 +45,12 @@ const TableDataPreparation = ({ examPrepList }: Props) => {
 
     return '';
   };
-
   const columns: TableColumn<DataRow>[] = [
     {
       name: 'No',
       selector: (row) => row.idx,
       sortable: true,
       width: '65px',
-    },
-    {
-      name: 'Tanggal Pembuatan',
-      selector: (row) => row.createdDate,
-      sortable: true,
-      wrap: true,
-      width: '170px',
-    },
-    {
-      name: 'Tipe',
-      selector: (row) => row.type,
-      sortable: true,
-    },
-    {
-      name: 'Penguji',
-      selector: (row) => row.accessor,
-      sortable: true,
-      wrap: true,
     },
     {
       name: 'Status',
@@ -67,12 +61,108 @@ const TableDataPreparation = ({ examPrepList }: Props) => {
       ),
     },
     {
+      name: 'Tipe',
+      selector: (row) => row.type,
+      sortable: true,
+      wrap: true,
+      width: '100px',
+    },
+    {
+      name: 'Prosedur',
+      selector: (row) => row.procedure,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Komentar',
+      selector: (row) => row.reason,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Lokasi',
+      selector: (row) => row.approvalLocation,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Kesulitan',
+      selector: (row) => row.approvalDifficulty,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Supervisi',
+      selector: (row) => row.approvalSupervision,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Self Evaluation',
+      selector: (row) => row.selfEvaluation,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Berjalan Dengan Baik',
+      selector: (row) => row.isGoingWell,
+      sortable: true,
+      cell: (row) => (row.isGoingWell === 'Yes' ? 'Ya' : 'Tidak'),
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Refleksi Pribadi',
+      selector: (row) => row.selfReflection,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Tipe Operasi',
+      selector: (row) =>
+        row.operationTypes
+          .map((operation) => operation.operationTypeName)
+          .join(', '),
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Rating',
+      selector: (row) => row.approvalGlobalRating,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Penguji',
+      selector: (row) => row.accessor,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
+      name: 'Tanggal Pembuatan',
+      selector: (row) => row.createdDate,
+      sortable: true,
+      wrap: true,
+      width: '150px',
+    },
+    {
       name: 'Approval Date',
       selector: (row) => row.approvalDate,
       sortable: true,
+      wrap: true,
+      width: '150px',
     },
   ];
-
   const data = useMemo(() => {
     return examPrepList
       ?.sort(
@@ -82,13 +172,26 @@ const TableDataPreparation = ({ examPrepList }: Props) => {
       ?.map((exam, idx) => {
         return {
           idx: idx + 1,
-          type: exam.type,
-          accessor: exam.assessorName,
+          type: exam.type || '',
+          procedure: exam.procedure || '',
+          reason: exam.reason || '',
+          approvalLocation: exam.approvalLocation || '',
+          approvalDifficulty: exam.approvalDifficulty || '',
           approvalDate:
             exam.approvalStatus === 'PENDING'
               ? '-'
               : convertDateForExam(exam.approvalConfirmDate),
-          status: exam.approvalStatus,
+          approvalSupervision: exam.approvalSupervision || '',
+          preparationDate: convertDateForExam(exam.preparationDate) || '',
+          selfEvaluation: exam.selfEvaluation || '',
+          isGoingWell: exam.isGoingWell ? 'Yes' : 'No',
+          selfReflection: exam.selfReflection || '',
+          approvalGlobalRating: exam.approvalGlobalRating || '',
+          approvalFeedbackGiven: exam.approvalFeedbackGiven || '',
+          operationTypes: exam.operationTypes || [],
+          additionalTags: exam.additionalTags || [],
+          accessor: exam.assessorName || '',
+          status: exam.approvalStatus || '',
           createdDate: convertDateForExam(exam.createdDate),
         };
       });
